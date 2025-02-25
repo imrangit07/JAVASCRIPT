4a226929ea85e4ef0a13748002a7ec79bd351167
imrangit07 <imranhussain21309@gmail.com>
2025-02-25 16:19:46 +0530

    Change h1 text using onclick

--- DOM/Events.js
+++ DOM/Events.js
@@ -1,3 +1,17 @@
+let clicked = document.getElementById("clicked")
+
 function Click(){
-    alert("hello, I am Clicked");
+    // alert("hello, I am Clicked");
+    clicked.textContent = "Zero";
+}
+function dblClick(){
+    // alert("hello, I am dbl. Clicked");
+    clicked.textContent = "Hero";
+
+}
+function enterMouse(){
+    alert("hello, Mouse Entered");
+}
+function leveMouse(){
+    alert("hello, Mouse Leaved");
 }
