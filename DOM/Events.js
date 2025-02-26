52df878dce3778b638accdb4efef2c25c6511442
imrangit07 <imranhussain21309@gmail.com>
2025-02-26 18:06:44 +0530

    Add task file

--- DOM/Events.js
+++ DOM/Events.js
@@ -15,3 +15,25 @@ function enterMouse(){
 function leveMouse(){
     alert("hello, Mouse Leaved");
 }
+
+
+//This is for background changes
+function red(){
+    document.body.style.backgroundColor = "red";
+    clicked.style.color = "#fff";
+    clicked.style.backgroundColor = "blue";
+}
+function yellow(){
+    document.body.style.backgroundColor = "yellow";
+    clicked.style.color = "#fff";
+    clicked.style.backgroundColor = "red";
+
+
+}
+function blue(){
+    document.body.style.backgroundColor = "blue";
+    clicked.style.color = "#000";
+    clicked.style.backgroundColor = "yellow";
+
+
+}
