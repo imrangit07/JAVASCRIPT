22d187b9ace53a5c2d524ca74ab1bc9cae5fc844
imrangit07 <imranhussain21309@gmail.com>
2025-03-06 16:22:13 +0530

    Add email Required and includes must have @

--- DOM/formHandling/formReq.js
+++ DOM/formHandling/formReq.js
@@ -1,25 +1,29 @@
 
 
-function inputReq(){
+function inputReq() {
     let name = document.querySelector("#Name").value;
     let email = document.querySelector("#Email").value;
     let age = document.querySelector("#Age").value;
 
     console.log("clicked");
-    
 
-    if(name==""){
+
+    if (name == "") {
         // alert("please enter your name");
         let message = document.querySelector("#errorMessage");
-        message.textContent="Please Enter Your Name";
+        message.textContent = "Please Enter Your Name";
         document.querySelector("#Name").focus();
         return false;
-    }else if(email==""){
+    } else if (email == "") {
         alert("please enter your email");
         document.querySelector("#Email").focus();
-
         return false;
-    }else if(age==""){
+    } else if (!(email.includes('@'))) {
+        alert("please enter your valide email and must be @");
+        document.querySelector("#Email").focus();
+        return false;
+    }
+    else if (age == "") {
         alert("please enter your age");
         document.querySelector("#Age").focus();
 
