fb3efdbf3cfb6ac2a638aaaf6ec9c90850c7e3de
imrangit07 <imranhussain21309@gmail.com>
2025-03-06 16:30:12 +0530

    Required Only Number in age input

--- DOM/formHandling/formReq.js
+++ DOM/formHandling/formReq.js
@@ -4,21 +4,21 @@ function inputReq() {
     let name = document.querySelector("#Name").value;
     let email = document.querySelector("#Email").value;
     let age = document.querySelector("#Age").value;
-
     console.log("clicked");
-
 
     if (name == "") {
         // alert("please enter your name");
+
         let message = document.querySelector("#errorMessage");
+        message.style.display= "block";
         message.textContent = "Please Enter Your Name";
         document.querySelector("#Name").focus();
         return false;
     } else if (email == "") {
         alert("please enter your email");
         document.querySelector("#Email").focus();
-        return false;
-    } else if (!(email.includes('@'))) {
+        return false;//false
+    } else if (!(email.includes('@'))) {//includes
         alert("please enter your valide email and must have @");
         document.querySelector("#Email").focus();
         return false;
@@ -26,7 +26,10 @@ function inputReq() {
     else if (age == "") {
         alert("please enter your age");
         document.querySelector("#Age").focus();
-
+        return false;
+    }else if(isNaN(age)){
+        alert("Please Enter Only Number")
+        document.querySelector("#Age").focus();
         return false;
     }
 }
