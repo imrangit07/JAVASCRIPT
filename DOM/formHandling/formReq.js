933be01cc65a52bd8b87a4a2f64f00136e526931
imrangit07 <imranhussain21309@gmail.com>
2025-03-06 16:15:42 +0530

    Add Form Handler files

---
+++ DOM/formHandling/formReq.js
@@ -0,0 +1,28 @@
+
+
+function inputReq(){
+    let name = document.querySelector("#Name").value;
+    let email = document.querySelector("#Email").value;
+    let age = document.querySelector("#Age").value;
+
+    console.log("clicked");
+    
+
+    if(name==""){
+        // alert("please enter your name");
+        let message = document.querySelector("#errorMessage");
+        message.textContent="Please Enter Your Name";
+        document.querySelector("#Name").focus();
+        return false;
+    }else if(email==""){
+        alert("please enter your email");
+        document.querySelector("#Email").focus();
+
+        return false;
+    }else if(age==""){
+        alert("please enter your age");
+        document.querySelector("#Age").focus();
+
+        return false;
+    }
+}
