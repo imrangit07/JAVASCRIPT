3cafe1ecd9672933f36a80e70ff93bc6cdd5f5ec
imrangit07 <imranhussain21309@gmail.com>
2025-03-07 14:10:50 +0530

    Add input details print function

--- DOM/formHandling/formReq.js
+++ DOM/formHandling/formReq.js
@@ -7,38 +7,52 @@ function inputReq() {
     console.log(typeof age);
     console.log(age.length);
 
-    if (name == "") {
+    if (name == "") {//check name is empty or not
         // alert("please enter your name");
-
         let message = document.querySelector("#errorMessage");
         message.style.display = "block";
         message.textContent = "Please Enter Your Name";
         document.querySelector("#Name").focus();
         return false;
     }
-    else if (email == "") {
+
+    else if (email == "") {//check email is empty or not
         alert("please enter your email");
         document.querySelector("#Email").focus();
         return false;//false
     }
-    else if (!(email.includes('@'))) {//includes
-        alert("please enter your valide email and must have @");
+
+    else if (!(email.includes('@')) || (!(email.includes('yahoo')) && !(email.includes('gmail')))) {//check @ and email/yahoo is included or not
+        alert("please enter your valide ID and must have @ && Gmail/Yahoo");
         document.querySelector("#Email").focus();
         return false;
     }
-    else if (age == "") {
+
+    else if (age == "") {//check age is empty or not
         alert("please enter your age");
         document.querySelector("#Age").focus();
         return false;
     }
-    else if (isNaN(age)) {
-        alert("Please Enter Only Number : " + age);
+
+    else if (isNaN(age)) {//check age is Number or not
+        alert(`"${age}" is Not a Number`);
         document.querySelector("#Age").focus();
         return false;
     }
-    else if (age.length > 3 || age <= 0) {
-        alert("This is not a valide Age : " + age);
+
+    else if (age.length > 3 || age <= 0) {//check age length is 3 only and age is not smaller then 0
+        alert(age + " is not a valide Age. ");
         document.querySelector("#Age").focus();
         return false;
     }
+
+    PrintDetails(name, email, age);//Call function for printiog all details and passe arguments
+}
+
+function PrintDetails(name, email, age) { // This function for printing input details 
+    let box = document.querySelector(".box");
+    box.innerHTML = `<b>Name : </b> ${name}<br><br>
+    <b>Email id: </b> ${email}<br><br>
+    <b>Age : </b> ${age} <br><br>
+    `;
 }
